29772760c635c783f1c555073977d33a8ea0f047
davidnguyen27 <theanhngujen2702@gmail.com>
2025-04-04 11:48:14 +0700

    Fixing time error

--- utils/TimeUtil.ts
+++ utils/TimeUtil.ts
@@ -8,9 +8,8 @@ const convertTo24Hour = (time: string): number => {
   return hour;
 };
 
-// Kiểm tra xem thời gian có bị vô hiệu hóa không khi chọn "Today"
 export const isTimeDisabled = (time: string, isToday: boolean): boolean => {
-  if (!isToday) return false; // Không disable nếu không phải "Today"
+  if (!isToday) return false;
 
   const now = new Date();
   const currentHour = now.getHours();
@@ -18,9 +17,7 @@ export const isTimeDisabled = (time: string, isToday: boolean): boolean => {
 
   const hour24 = convertTo24Hour(time);
 
-  // Disable nếu thời gian nhỏ hơn hoặc bằng thời gian hiện tại
   return hour24 < currentHour || (hour24 === currentHour && currentMinute > 0);
 };
 
-// Danh sách các khung giờ cố định
-export const timeSlots = ["9:00 AM", "10:00 AM", "11:00 AM", "14:00 PM", "15:00 PM", "16:00 PM", "17:00 PM"];
+export const timeSlots = ["9:00 AM", "10:00 AM", "11:00 AM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"];
